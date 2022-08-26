bfd4c658aa31006d875d42b5c668ac94f2128530
7IEH <youkitea@naver.com>
2022-08-26 21:48:42 +0900

    2022/08/26
    1. Title화면에 이동 버튼 추가
    : start_scene 이동 버튼, tool_scene 이동 버튼, window 종료 버튼

--- WinApi_basic/CScene_Title.cpp
+++ WinApi_basic/CScene_Title.cpp
@@ -3,9 +3,16 @@
 
 #include "CKeyMgr.h"
 #include "CObject.h"
+#include "CObject_UI.h"
+#include "CObject_BtnUI.h"
 #include "Background_Title.h"
 
 #include "CCollisionMgr.h"
+#include "CCore.h"
+
+void StartScene(DWORD_PTR, DWORD_PTR);
+void ToolScene(DWORD_PTR, DWORD_PTR);
+void WindowExit(DWORD_PTR, DWORD_PTR);
 
 CScene_Title::CScene_Title()
 {
@@ -18,10 +25,10 @@ CScene_Title::~CScene_Title()
 void CScene_Title::update()
 {
 	CScene::update();
-	if (KEY_CHECK(ENTER, TAP)) 
-	{
-		ChangeScene(SCENE_TYPE::START);
-	}
+	//if (KEY_CHECK(ENTER, TAP)) 
+	//{
+	//	//ChangeScene(SCENE_TYPE::START);
+	//}
 }
 
 void CScene_Title::Enter()
@@ -29,6 +36,24 @@ void CScene_Title::Enter()
 	CObject* bObj = new Background_Title;
 	bObj->SetPos(vec2(640.f, 384.f));
 	pushObject((UINT)GROUP_TYPE::DEFAULT,bObj);
+
+	CObject_BtnUI* BtnObj = new CObject_BtnUI;
+	BtnObj->SetPos(vec2(940.f, 400.f));
+	BtnObj->SetScale(vec2(300.f, 50.f));
+	BtnObj->SetClickedCallBack(StartScene, 0, 0);
+	pushObject((UINT)GROUP_TYPE::UI,BtnObj);
+
+	CObject_BtnUI* BtnObj2 = new CObject_BtnUI;
+	BtnObj2->SetPos(vec2(940.f, 490.f));
+	BtnObj2->SetScale(vec2(300.f, 50.f));
+	BtnObj2->SetClickedCallBack(ToolScene, 0, 0);
+	pushObject((UINT)GROUP_TYPE::UI, BtnObj2);
+
+	CObject_BtnUI* BtnObj3 = new CObject_BtnUI;
+	BtnObj3->SetPos(vec2(940.f, 580.f));
+	BtnObj3->SetScale(vec2(300.f, 50.f));
+	BtnObj3->SetClickedCallBack(WindowExit, 0, 0);
+	pushObject((UINT)GROUP_TYPE::UI, BtnObj3);
 }
 
 void CScene_Title::Exit()
@@ -38,4 +63,17 @@ void CScene_Title::Exit()
 	CCollisionMgr::GetInst()->Reset();
 }
 
+void StartScene(DWORD_PTR, DWORD_PTR)
+{
+	ChangeScene(SCENE_TYPE::START);
+}
 
+void ToolScene(DWORD_PTR, DWORD_PTR)
+{
+	ChangeScene(SCENE_TYPE::TOOL);
+}
+
+void WindowExit(DWORD_PTR, DWORD_PTR)
+{
+	DestroyWindow(CCore::GetInst()->getHWND());
+}
